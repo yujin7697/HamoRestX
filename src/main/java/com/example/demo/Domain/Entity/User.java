6f50c2da034e5c6da3066f555592dc092795287e
kimgyeongmin123 <orange761943@naver.com>
2023-12-11 21:20:39 +0900

    비밀번호암호화문제

--- src/main/java/com/example/demo/Domain/Entity/User.java
+++ src/main/java/com/example/demo/Domain/Entity/User.java
@@ -2,6 +2,7 @@ package com.example.demo.Domain.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
+@Table(name="user")
 public class User {
     @Id
     private String userId;
