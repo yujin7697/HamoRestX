b7bc576f1ea876bdd863a695ab8a1782a74818d1
kimgyeongmin123 <orange761943@naver.com>
2023-12-18 14:21:04 +0900

    관리자페이지

--- src/main/java/com/example/demo/Controller/MasterController.java
+++ src/main/java/com/example/demo/Controller/MasterController.java
@@ -1,4 +1,20 @@
 package com.example.demo.Controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
 public class MasterController {
+
+    @GetMapping("/admin/adminMain")
+    public void adminMain() {
+
+    }
+
+    @GetMapping("/admin/hotelPermit")
+    public void hotelPermit() {
+
+    }
 }
