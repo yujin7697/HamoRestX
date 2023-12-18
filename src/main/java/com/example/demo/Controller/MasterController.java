b4b9185994360e50603c1926459f88a1bb57f623
kimgyeongmin123 <orange761943@naver.com>
2023-12-18 14:37:15 +0900

    춥다집갈래

--- src/main/java/com/example/demo/Controller/MasterController.java
+++ src/main/java/com/example/demo/Controller/MasterController.java
@@ -1,12 +1,21 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Domain.Entity.Hotel;
+import com.example.demo.Domain.Service.HotelService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
 public class MasterController {
+
+    @Autowired
+    private HotelService hotelService;
 
     @GetMapping("/admin/adminMain")
     public String adminMain() {
@@ -16,7 +25,11 @@ public class MasterController {
     }
 
     @GetMapping("/admin/hotelPermit")
-    public String hotelPermit() {
+    public String hotelPermit(Model model) {
+
+        //모든 숙소 리스트 가져오기
+        List<Hotel> hotelList = hotelService.getHotelList();
+        model.addAttribute("hotels", hotelList);
 
         return "/admin/hotelPermit";
 
